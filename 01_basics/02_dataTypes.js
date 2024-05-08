71978241a6e45ffa1b78ff7797224d6a222ee4db
Mayank Lodaya <lodaya.mayank@gmail.com>
2024-05-08 11:44:48 +0000

    Chapter 2 datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+// alert(1+1) // we are using nodejs, not browser  
+
+//number
+//bigint => to store large values
+//string => "" defined using double quotes
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
