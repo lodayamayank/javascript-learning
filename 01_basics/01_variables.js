83c871e67870769be65a67cf3596cd97f7b48da2
Mayank Lodaya <lodaya.mayank@gmail.com>
2024-05-08 11:28:46 +0000

    first lesson

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const  accountId = 46554
+let accountEmail = "lodaya.mayank@gmail.com"
+var accountPassword = "213456"
+accountCity = "Brsbane"
+
+//accountId = 313 //not allowed
+
+accountEmail = "mayank@google.com"
+accountPassword = "46546"
+accountCity = "Melbourne"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountCity,accountPassword]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
