a105c5e405a2a971fa44b978458cecf6a1b8d4f4
Mayank Lodaya <lodaya.mayank@gmail.com>
2024-05-09 11:19:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log( typeof valueInNumber)
+
+//"33" => 33
+// "33abc" => Nan
+// true=>1; false=>0
+
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+//1=>true ; 0=>false
+//""=>false
+// "abc" => true
+
+let x = 43
+let stringNumber = String(x)
+console.log(stringNumber);
+console.log(typeof stringNumber);
